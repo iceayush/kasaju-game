cbb4540fe5e0b7262417bedf57eb11483d220852
iceayush <ayushshresthalakhe@gmail.com>
2026-02-13 02:28:48 -0600

    Restore project files

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/kasaju-game/', // <--- important
+  base: '/kasaju-game/',  // <-- important for GitHub Pages
   plugins: [react()],
 });
