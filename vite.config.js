de3220ded194a30508d2eb486c5b49ff54525b16
iceayush <ayushshresthalakhe@gmail.com>
2026-02-13 02:01:43 -0600

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/kasaju-game/',  // <-- important for GitHub Pages
   plugins: [react()],
-})
+});
